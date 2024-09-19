0c45b09d47313a20a6901b6bcc52889459203c5d
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2024-09-18 22:05:52 -0300

    Initial commit

---
+++ src/main/java/com/zodus/questionize/QuestionizeApplication.java
@@ -0,0 +1,13 @@
+package com.zodus.questionize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zodus/questionize/QuestionizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zodus.questionize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
