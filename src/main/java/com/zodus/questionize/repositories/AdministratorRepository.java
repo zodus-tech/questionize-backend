2b697858669681b098d8e59cff0ba92a0a5d4bd4
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2024-10-08 19:51:01 -0300

    (main): create repositories for previous models

---
+++ src/main/java/com/zodus/questionize/repositories/AdministratorRepository.java
@@ -0,0 +1,11 @@
+package com.zodus.questionize.repositories;
+
+import com.zodus.questionize.models.Administrator;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AdministratorRepository extends JpaRepository<Administrator, UUID> {
+}
