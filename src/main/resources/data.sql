6fe2e7513408c1d28631abefe702c6bed0b6a8a3
joao-erik2077 <joaoerik2074@gmail.com>
2024-11-07 20:54:24 -0300

    (main): solve conflict with older mysql versions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT IGNORE INTO `department` (`id`, `name`)
-    VALUES (UUID_TO_BIN(UUID()), "Admin");
+    VALUES (UUID(), "Admin");
 
+-- senha admin123
 INSERT IGNORE INTO `administrator` (`id`, `name`, `password`, `username`, `department_id`)
-    VALUES(UUID_TO_BIN(UUID()), "Administrator", "$2a$12$4F5OCej4ao6lb4mba3jIV.4d.VdiT7OSqSM6u/dWKLtrNWEC.2jT2", "admin", (SELECT `id` FROM `department` WHERE `name` = "Admin" LIMIT 1));
+    VALUES(UUID(), "Administrator", "$2a$12$4F5OCej4ao6lb4mba3jIV.4d.VdiT7OSqSM6u/dWKLtrNWEC.2jT2", "admin", (SELECT `id` FROM `department` WHERE `name` = "Admin" LIMIT 1));
